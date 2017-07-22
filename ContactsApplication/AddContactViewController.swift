a639690f6111ff1dab0055ee984434e692defc91
Kyle <kalm42@gmail.com>
2017-07-21 22:15:23 -0700

    Initial commit

---
+++ ContactsApplication/AddContactViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AddContactViewController.swift
+//  ContactsApplication
+//
+//  Created by Kyle Melton on 4/29/17.
+//  Copyright © 2017 Kyle Melton. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class AddContactViewController: UIViewController {
+    var titleText = "Add Contact"
+    var contact: NSManagedObject? = nil
+    var indexPathForContact: IndexPath? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = titleText
+        if let contact = self.contact {
+            nameTextField.text = contact.value(forKey: "name") as? String
+            phoneNumberTextField.text = contact.value(forKey: "phoneNumber") as? String
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+
+    // MARK: - Navigation
+
+    @IBAction func saveAndClose(_ sender: Any) {
+        performSegue(withIdentifier: "unwindToContactList", sender: self)
+    }
+    @IBAction func close(_ sender: Any) {
+        nameTextField.text = nil
+        phoneNumberTextField.text = nil
+        performSegue(withIdentifier: "unwindToContactList", sender: self)
+    }
+    
+    
+}
---
+++ ContactsApplication/ContactDetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ContactDetailViewController.swift
+//  ContactsApplication
+//
+//  Created by Kyle Melton on 4/29/17.
+//  Copyright © 2017 Kyle Melton. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ContactDetailViewController: UIViewController {
+    
+    var contact: NSManagedObject? = nil
+    var isDeleted: Bool = false
+    var indexPath: IndexPath? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameLabel.text = contact?.value(forKey:"name") as? String
+        phoneLabel.text = contact?.value(forKey:"phoneNumber") as? String
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+
+    @IBAction func done(_ sender: Any) {
+        performSegue(withIdentifier: "unwindToContactList", sender: self)
+    }
+    
+    @IBAction func deleteContact(_ sender: Any) {
+        isDeleted = true
+        performSegue(withIdentifier: "unwindToContactList", sender: self)
+    }
+    
+    //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "editContact" {
+            guard let viewController = segue.destination as? AddContactViewController else { return }
+            viewController.titleText = "Edit Contact"
+            viewController.contact = contact
+            viewController.indexPathForContact = self.indexPath!
+        }
+    }
+    
+}
